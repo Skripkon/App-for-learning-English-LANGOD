c9950b47ccb1ed4992adf2bec6c843f1d4af03a8
Skripkon <78466953+Skripkon@users.noreply.github.com>
2023-03-13 00:40:35 +0300

    Update Word.py

--- Word.py
+++ Word.py
@@ -1,3 +1,7 @@
+from gtts import gTTS
+from playsound import playsound
+import requests
+
 class Word:
     current_word: str = ""
         
@@ -16,22 +20,22 @@ class Word:
     def get_the_pronunciation_of_a_word_with_American_accent(cls):
         pronunciation_of_a_word_with_American_accent = gTTS(cls.current_word, tld="us")
         file: str = cls.current_word + "US.mp3"
-        USA.save('USA.mp3')
-        playsound("USA.mp3")
+        pronunciation_of_a_word_with_American_accent.save(file)
+        playsound(file)
     
     
     @classmethod
     def get_the_pronunciation_of_a_word_with_British_accent(cls):
-        pronunciation_of_a_word_with_British_accen = gTTS(cls.current_word, tld="en")
+        pronunciation_of_a_word_with_British_accent = gTTS(cls.current_word, tld="co.uk")
         file: str = cls.current_word + "UK.mp3"
-        USA.save('UK.mp3')
-        playsound("UK.mp3")
+        pronunciation_of_a_word_with_British_accent.save(file)
+        playsound(file)
     
     
     @classmethod
     def get_the_video_with_a_word(cls):
         phraseEncoded = ""
-        for char in phrase:
+        for char in cls.current_word:
             if (char == ' '):
                 phraseEncoded += "%20"
             elif (char == "'"):
@@ -52,6 +56,7 @@ class Word:
             index += 1
         new_request_URL: str = "https://y.yarn.co/" + video_url + ".mp4"    
         new_request = requests.get(new_request_URL)
-        file=open(r'file.mp4',"wb")
+        name: str = cls.current_word + ".mp4"
+        file=open(name,"wb")
         file.write(new_request.content)
         file.close()
