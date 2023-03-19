36b08f9978a76758a7bb218854d4408ee1507113
Skripkon <78466953+Skripkon@users.noreply.github.com>
2023-03-19 22:01:20 +0300

    Create DataBase.py

---
+++ DataBase.py
@@ -0,0 +1,41 @@
+import sqlite3
+import os.path
+
+class DataBase:
+    
+    @classmethod
+    def create_data_base(cls, name):    
+        
+        if name == "registration_info":
+            sqlite_create_table_query = '''CREATE TABLE registration_info (
+                                login TEXT PRIMARY KEY,
+                                password TEXT NOT NULL,
+                                id INTEGER NOT NULL UNIQUE);'''
+            
+        elif name == "users_data":
+            columns: str = ""
+            for i in range(500):
+                columns += "word" + str(i) + ' TEXT, '  
+            columns += "word500 TEXT"
+            
+            sqlite_create_table_query = f'''CREATE TABLE users_data (
+                                id INTEGER PRIMARY KEY,
+                                amount_of_words INTEGER,
+                                {columns});'''
+        
+        sqlite_connection = sqlite3.connect(f'{name}.db') 
+        cursor = sqlite_connection.cursor()
+        try:
+            cursor.execute(sqlite_create_table_query)
+        except sqlite3.Error as error:
+            print("BD has already created")
+            
+            
+    @classmethod    
+    def chech_whether_data_bases_exist(cls):
+        
+        if os.path.exists("users_data.db") == False:
+            create_data_base("users_data")
+            
+        if os.path.exists("registration_info.db") == False:
+             create_data_base("registration_info")
