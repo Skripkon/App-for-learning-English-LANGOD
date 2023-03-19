d07417a5fd292f37b9ce65cec33d8cd356786f8a
Nick Skripko <nskripko@icloud.com>
2023-03-19 23:33:36 +0300

    DataBase updated2

--- DataBase.py
+++ DataBase.py
@@ -2,37 +2,52 @@ import sqlite3
 import os.path
 
 class DataBase:
+    sqlite_connection = None
+    cursor = None
+
     @classmethod
     def create_data_base(cls, name):
         global sqlite_create_table_query
         if name == "registration_info":
             sqlite_create_table_query = '''CREATE TABLE registration_info (
-                                login TEXT PRIMARY KEY,
-                                password TEXT NOT NULL,
-                                id INTEGER NOT NULL UNIQUE);'''
-            
+                                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                login TEXT UNIQUE NOT NULL,
+                                password TEXT NOT NULL);'''
+
         elif name == "users_data":
             columns: str = ""
             for i in range(500):
-                columns += "word" + str(i) + ' TEXT, '  
+                columns += "word" + str(i) + ' TEXT, '
             columns += "word500 TEXT"
-            
+
             sqlite_create_table_query = f'''CREATE TABLE users_data (
                                 id INTEGER PRIMARY KEY,
                                 amount_of_words INTEGER,
                                 {columns});'''
-        
-        sqlite_connection = sqlite3.connect(f'{name}.db') 
+
+        sqlite_connection = sqlite3.connect(f'{name}.db')
         cursor = sqlite_connection.cursor()
         try:
             cursor.execute(sqlite_create_table_query)
         except sqlite3.Error as error:
             print("BD has already created")
-            
-            
-    @classmethod    
-    def chech_whether_data_bases_exist(cls):
-        if os.path.exists("users_data.db") == False:
+
+
+    @classmethod
+    def check_whether_data_bases_exist(cls):
+        if not os.path.exists("users_data.db"):
             cls.create_data_base("users_data")
-        if os.path.exists("registration_info.db") == False:
-             cls.create_data_base("registration_info")
+        if not os.path.exists("registration_info.db"):
+            cls.create_data_base("registration_info")
+        cls.sqlite_connection = sqlite3.connect('registration_info.db')
+        cls.cursor = cls.sqlite_connection.cursor()
+
+    @classmethod
+    def create_user(cls, new_login: str, new_password: str):
+        cls.cursor.execute("INSERT INTO registration_info (login, password) VALUES(?, ?)", (new_login, new_password))
+        cls.sqlite_connection.commit()
+
+    @classmethod
+    def close_connection(cls):
+        cls.cursor.close()
+        cls.sqlite_connection.close()
