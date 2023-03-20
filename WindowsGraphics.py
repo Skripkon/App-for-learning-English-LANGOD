6828c4df353bc37d3f4ee79f2eeb80ad2c3f685a
Nick Skripko <nskripko@icloud.com>
2023-03-20 18:01:49 +0300

    BetaVersion1.1

--- WindowsGraphics.py
+++ WindowsGraphics.py
@@ -2,7 +2,8 @@ import sys
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QDialog, QApplication, QMainWindow, QMessageBox, QVBoxLayout, QHBoxLayout, QSpacerItem, QSizePolicy
 from PyQt5.uic import loadUi
-
+import sqlite3
+import DataBase
 
 class Login(QDialog):
     def __init__(self):
@@ -13,19 +14,29 @@ class Login(QDialog):
         self.createaccbutton.clicked.connect(self.gotocreate)
 
     def loginfunction(self):
-        email = self.email.text()
+        DataBase.DataBase.check_whether_data_bases_exist()
+        login_text = self.email.text()
         password = self.password.text()
-        if email == 'a' and password == 'p':
+        if DataBase.DataBase.check_if_user_exists(login_text, password) != -1:
             self.accept()
             self.window = MainWindow()
             self.window.show()
         else:
-            print("Wrong username or password")
+            self.open_the_window("ERROR", "Such user hasn't found")
+
 
     def gotocreate(self):
         createacc = CreateAcc()
         widget.addWidget(createacc)
         widget.setCurrentIndex(widget.currentIndex() + 1)
+
+    def open_the_window(self, title_of_the_window: str, information: str):
+        msg_box = QtWidgets.QMessageBox()
+        msg_box.setText(information)
+        msg_box.setWindowTitle(title_of_the_window)
+        msg_box.setIcon(QtWidgets.QMessageBox.Information)
+        msg_box.setStandardButtons(QtWidgets.QMessageBox.Ok)
+        msg_box.exec_()
 
 
 class CreateAcc(QDialog):
@@ -36,19 +47,42 @@ class CreateAcc(QDialog):
         self.password.setEchoMode(QtWidgets.QLineEdit.Password)
         self.confirmpass.setEchoMode(QtWidgets.QLineEdit.Password)
 
+
+    @classmethod
+    def correct_password(cls, password: str) -> str:
+        # TODO
+        pass
+
     def createaccfunction(self):
-        email = self.email.text()
+        DataBase.DataBase.check_whether_data_bases_exist()
+        login_text = self.email.text()
         if self.password.text() == self.confirmpass.text():
             password = self.password.text()
-            msg_box = QtWidgets.QMessageBox()
-            msg_box.setText("Krasavchik")
-            msg_box.setWindowTitle("Success")
-            msg_box.setIcon(QtWidgets.QMessageBox.Information)
-            msg_box.setStandardButtons(QtWidgets.QMessageBox.Ok)
-            msg_box.exec_()
+            IsPasswordCorrect: str = self.correct_password(password)
+            if IsPasswordCorrect != "NO ERROR":
+                self.open_the_window("Error", IsPasswordCorrect)
+                return None
+            try:
+                DataBase.DataBase.create_user(login_text, password)
+            except sqlite3.Error:
+                self.open_the_window("Error", "User with this login already exists")
+                return None
+            self.open_the_window("OK", "Have fun using our app!")
             login = Login()
             widget.addWidget(login)
             widget.setCurrentIndex(widget.currentIndex() + 1)
+        else:
+            self.open_the_window("Error", "Passwords aren't the same")
+
+    def open_the_window(self, title_of_the_window: str, information: str):
+        msg_box = QtWidgets.QMessageBox()
+        msg_box.setText(information)
+        msg_box.setWindowTitle(title_of_the_window)
+        msg_box.setIcon(QtWidgets.QMessageBox.Information)
+        msg_box.setStandardButtons(QtWidgets.QMessageBox.Ok)
+        msg_box.exec_()
+        self.password.clear()
+        self.confirmpass.clear()
 
 
 class MainWindow(QMainWindow):
